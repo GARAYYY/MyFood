96f6bdcd140262764a0a5bfbd47b9a43b20f7de4
Jon <jongaray7@gmail.com>
2025-08-22 21:12:44 +0200

    Added /register to web.php

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('pages.LoginView');
 });
+
+Route::get('/register', function () {
+    return view('pages.RegisterView');
+});
