1f4a3dfb4c6f4eed571b93e847cbf7e20ea2af70
Jon <jongaray7@gmail.com>
2025-08-25 20:35:17 +0200

    HomeView restart

--- resources/views/pages/HomeView.blade.php
+++ resources/views/pages/HomeView.blade.php
@@ -11,66 +11,48 @@
 @include('components.Footer')
 
 <style>
-    .cards-container {
-        margin-top: 1rem;
-        display: flex;
-        flex-wrap: wrap;
-        justify-content: center;
-        gap: 1rem;
-        padding: 0 1rem;
-    }
+.cards-container {
+    margin-top: 1rem;
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+    gap: 1rem;
+    padding: 0 3rem;
+}
 
     .recipe-link {
         text-decoration: none;
         color: inherit;
+        width: fit-content;
         display: block;
-        flex: 1 1 calc(33.333% - 1rem);
-        max-width: calc(33.333% - 1rem);
     }
 
-    @media (max-width: 992px) {
-        .recipe-link {
-            flex: 1 1 calc(50% - 1rem);
-            max-width: calc(50% - 1rem);
-        }
-    }
+.floating-add {
+    position: fixed;
+    bottom: 20px;
+    right: 20px;
+    background: #70A9A1;
+    border: none;
+    cursor: pointer;
+    z-index: 999;
+    border-radius: 100%;
+}
 
-    @media (max-width: 480px) {
-        .cards-container {
-            flex-direction: column;
-            gap: 1rem;
-        }
+.floating-add img {
+    width: 50px;
+    height: 50px;
+    object-fit: contain;
+}
 
-        .cards-container>a {
-            width: 100%;
-        }
+.floating-add:hover {
+    transform: translateY(-3px) translateX(-3px);
+    border-bottom: 2px solid black;
+    border-right: 2px solid black;
+}
 
-        .recipe-link {
-            flex: 1 1 100%;
-            max-width: 100%;
-        }
-    }
-
-    .floating-add {
-        position: fixed;
-        bottom: 20px;
-        right: 20px;
-        background: #70A9A1;
-        border: none;
-        cursor: pointer;
-        z-index: 999;
-        border-radius: 100%;
-    }
-
-    .floating-add img {
-        width: 50px;
-        height: 50px;
-        object-fit: contain;
-    }
-
-    .floating-add:hover {
-        transform: translateY(-3px) translateX(-3px);
-        border-bottom: 2px solid black;
-        border-right: 2px solid black;
-    }
+.recipe-link {
+    text-decoration: none;
+    color: inherit;
+    width: fit-content;
+}
 </style>
