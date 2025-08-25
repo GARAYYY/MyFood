339e128740dc2208c3dd28656e4df8636e016931
Jon <jongaray7@gmail.com>
2025-08-25 20:29:04 +0200

    HomeView 1 row on mobiles

--- resources/views/pages/HomeView.blade.php
+++ resources/views/pages/HomeView.blade.php
@@ -11,22 +11,24 @@
 @include('components.Footer')
 
 <style>
-.cards-container {
-    margin-top: 1rem;
-    display: flex;
-    flex-wrap: wrap;
-    justify-content: center;
-    gap: 1rem;
-    padding: 0 3rem;
-}
+    .cards-container {
+        margin-top: 1rem;
+        display: flex;
+        flex-wrap: wrap;
+        justify-content: center;
+        gap: 1rem;
+        padding: 0 1rem;
+    }
 
     .recipe-link {
         text-decoration: none;
         color: inherit;
-        width: fit-content;
         display: block;
+        flex: 1 1 calc(33.333% - 1rem);
+        max-width: calc(33.333% - 1rem);
     }
 
+    /* Tablet: 2 columnas */
     @media (max-width: 992px) {
         .recipe-link {
             flex: 1 1 calc(50% - 1rem);
@@ -34,39 +36,34 @@
         }
     }
 
-    @media (max-width: 600px) {
+    /* Móvil: 1 columna */
+    @media (max-width: 480px) {
         .recipe-link {
             flex: 1 1 100%;
             max-width: 100%;
         }
     }
 
-.floating-add {
-    position: fixed;
-    bottom: 20px;
-    right: 20px;
-    background: #70A9A1;
-    border: none;
-    cursor: pointer;
-    z-index: 999;
-    border-radius: 100%;
-}
+    .floating-add {
+        position: fixed;
+        bottom: 20px;
+        right: 20px;
+        background: #70A9A1;
+        border: none;
+        cursor: pointer;
+        z-index: 999;
+        border-radius: 100%;
+    }
 
-.floating-add img {
-    width: 50px;
-    height: 50px;
-    object-fit: contain;
-}
+    .floating-add img {
+        width: 50px;
+        height: 50px;
+        object-fit: contain;
+    }
 
-.floating-add:hover {
-    transform: translateY(-3px) translateX(-3px);
-    border-bottom: 2px solid black;
-    border-right: 2px solid black;
-}
-
-.recipe-link {
-    text-decoration: none;
-    color: inherit;
-    width: fit-content;
-}
+    .floating-add:hover {
+        transform: translateY(-3px) translateX(-3px);
+        border-bottom: 2px solid black;
+        border-right: 2px solid black;
+    }
 </style>
