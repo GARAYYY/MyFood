1ae0bbb284bd44cc63bdd51b950443e7854c72f3
Jon <jongaray7@gmail.com>
2025-08-25 19:25:38 +0200

    HomeVies rwsponsive

--- resources/views/pages/HomeView.blade.php
+++ resources/views/pages/HomeView.blade.php
@@ -11,49 +11,61 @@
 @include('components.Footer')
 
 <style>
-.cards-container {
-    margin-top: 1rem;
-    display: flex;
-    flex-wrap: wrap;
-    justify-content: center;
-    gap: 1rem;
-    padding: 0 3rem;
-}
+    .cards-container {
+        margin-top: 1rem;
+        display: flex;
+        flex-wrap: wrap;
+        justify-content: center;
+        gap: 1rem;
+        padding: 0 1rem;
+    }
 
-.cards-container>* {
-    flex: 1 1 calc(33.333% - 1rem);
-    max-width: calc(33.333% - 1rem);
-}
+    /* Cada card ocupa 3 columnas en pantallas grandes */
+    .cards-container>a {
+        flex: 1 1 calc(33.333% - 1rem);
+        max-width: calc(33.333% - 1rem);
+        text-decoration: none;
+        color: inherit;
+        display: block;
+        /* Muy importante para que el link no desborde */
+    }
 
-.floating-add {
-    position: fixed;
-    bottom: 20px;
-    right: 20px;
-    background: #70A9A1;
-    border: none;
-    cursor: pointer;
-    z-index: 999;
-    border-radius: 100%;
-}
+    /* Tablet: 2 columnas */
+    @media (max-width: 992px) {
+        .cards-container>a {
+            flex: 1 1 calc(50% - 1rem);
+            max-width: calc(50% - 1rem);
+        }
+    }
 
-.floating-add img {
-    width: 50px;
-    height: 50px;
-    object-fit: contain;
-}
+    /* Móvil: 1 columna */
+    @media (max-width: 600px) {
+        .cards-container>a {
+            flex: 1 1 100%;
+            max-width: 100%;
+        }
+    }
 
-.floating-add:hover {
-    transform: translateY(-3px) translateX(-3px);
-    border-bottom: 2px solid black;
-    border-right: 2px solid black;
-}
+    .floating-add {
+        position: fixed;
+        bottom: 20px;
+        right: 20px;
+        background: #70A9A1;
+        border: none;
+        cursor: pointer;
+        z-index: 999;
+        border-radius: 100%;
+    }
 
-.recipe-link {
-    text-decoration: none;
-    color: inherit;
-}
+    .floating-add img {
+        width: 50px;
+        height: 50px;
+        object-fit: contain;
+    }
 
-.cards-container a{
-    width: 350px;
-}
+    .floating-add:hover {
+        transform: translateY(-3px) translateX(-3px);
+        border-bottom: 2px solid black;
+        border-right: 2px solid black;
+    }
 </style>
