b085aa3ff8868f33c09bbf30debb25a6c2f131b6
Jon <jongaray7@gmail.com>
2025-08-25 20:08:32 +0200

    Revert "Avoid zoom on mobiles"
    This reverts commit 10675fc6f6e1512eb30a35bb1fcf8696408d8453.

--- resources/views/components/Header.blade.php
+++ resources/views/components/Header.blade.php
@@ -63,14 +63,4 @@
         background-color: #E2F3E9;
         box-shadow: 5px 5px 10px black;
     }
-
-    @media (max-width: 600px) {
-
-        input,
-        textarea,
-        select,
-        button {
-            font-size: 16px;
-        }
-    }
 </style>
