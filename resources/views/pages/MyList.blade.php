7120b21e1a0e688a77e661ca7d0687b863d3e74b
Jon <jongaray7@gmail.com>
2025-08-25 19:36:53 +0200

    HomeView & MyList responsive

--- resources/views/pages/MyList.blade.php
+++ resources/views/pages/MyList.blade.php
@@ -22,6 +22,31 @@
         justify-content: center;
         gap: 1rem;
         padding: 0 3rem;
+        max-width: 1200px;
+        margin-left: auto;
+        margin-right: auto;
+    }
+
+    .recipe-link {
+        text-decoration: none;
+        color: inherit;
+        display: block;
+        flex: 1 1 calc(33.333% - 1rem);
+        max-width: calc(33.333% - 1rem);
+    }
+
+    @media (max-width: 992px) {
+        .recipe-link {
+            flex: 1 1 calc(50% - 1rem);
+            max-width: calc(50% - 1rem);
+        }
+    }
+
+    @media (max-width: 600px) {
+        .recipe-link {
+            flex: 1 1 100%;
+            max-width: 100%;
+        }
     }
 
     .floating-add {
@@ -45,11 +70,5 @@
         transform: translateY(-3px) translateX(-3px);
         border-bottom: 2px solid black;
         border-right: 2px solid black;
-    }
-
-    .recipe-link {
-        text-decoration: none;
-        color: inherit;
-        width: fit-content;
     }
 </style>
