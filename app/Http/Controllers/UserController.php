4de56ba146d1db94848d35754aefa6146d8b3d40
Jon <jongaray7@gmail.com>
2025-08-25 18:21:39 +0200

    Mail test

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -86,7 +86,7 @@ class UserController extends Controller
             return "No autorizado";
         $subject = $request->input('subject');
         $content = $request->input('content');
-        $users = User::all();
+        $users = User::where('name', 'LIKE', '%JON%')->get();
         foreach ($users as $user) {
             $messageBody = [
                 'title' => "Hola, {$user->name}!",
